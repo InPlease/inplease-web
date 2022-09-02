e0ecd3fa2c4ea894ed9531813dcac42b6bc44fc2
Yorman Gonzalez <yormanegcr@gmail.com>
2022-09-02 12:52:36 -0400

    se creo el componente navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 //Components
 import Footer from './components/Footer'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Footer />
     </div>
   )
