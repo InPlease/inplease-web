633315ef7b10832b150b3bd3bf652a4de17b46a9
karttofer <jhornancolina@gmail.com>
2022-09-03 10:11:50 -0500

    add router, photos, refactoring code and delete content in readme

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+// Dependencies
+import { Routes, Route } from 'react-router-dom'
+
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="about" element={<h1>About</h1>} />
+        <Route path="contact" element={<h1>Contact</h1>} />
+      </Routes>
+    </div>
+  )
 }
 
 export default App
