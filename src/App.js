c0f10297364d2a004a18f5b0bf3270cb8e1d8a11
Yorman Gonzalez <yormanegcr@gmail.com>
2022-09-09 12:56:45 -0400

    Implementacion del ourvoice

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 // Dependencies
 import { Routes, Route } from 'react-router-dom'
-
+import Ourvoice from './components/Ourvoice'
 function App() {
   return (
     <div className="App">
+      <Ourvoice />
       <Routes>
         <Route path="/" element={<h1>Home</h1>} />
         <Route path="about" element={<h1>About</h1>} />
