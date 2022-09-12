8090ae833ead449c5b017688dd6645e310e31fab
karttofer <jhornancolina@gmail.com>
2022-09-11 21:01:00 -0500

    Add the sucess message, resolve bug with useMenu and add the links route in the map

--- src/components/Sections/Contact/FormSuccessScreen.js
+++ src/components/Sections/Contact/FormSuccessScreen.js
@@ -1,12 +1,12 @@
 // Dependencies
 import disco__icon from '../../../assets/img/icon/disco-icon.svg';
 
-const FormSuccessScreen = ({ closeEvent }) => {
+const FormSuccessScreen = ({ closeEvent, translation }) => {
   return (
     <section className="form__success-screen" onClick={closeEvent}>
-      <img src={disco__icon} />
-      <h1>Ahooooy!</h1>
-      <p>Thank you for buying a seat, we will confirm the flight soon.</p>
+      <img src={disco__icon} alt="flying saucer" />
+      <h1>{translation.scream}</h1>
+      <p>{translation.information}</p>
     </section>
   );
 };
